0c798f2fab339c5d4b51084f1f8dac2a9012babc
Ashok <11200752@in.techwave.net>
2019-05-09 15:02:57 +0530

    Modifying docker ui

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, welcome to docker.</p>
+          Hi, welcome to docker world.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
